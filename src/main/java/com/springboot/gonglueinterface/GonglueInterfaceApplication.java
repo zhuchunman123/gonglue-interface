0b29f5731fd6cb1d26259b89e42c8f9c916e163e
fhrdr <1837211345@qq.com>
2021-07-21 10:10:51 +0800

    test

---
+++ src/main/java/com/springboot/gonglueinterface/GonglueInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gonglueinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonglueInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonglueInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/gonglueinterface/GonglueInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gonglueinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonglueInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
